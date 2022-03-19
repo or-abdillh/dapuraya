f98fdac6803df21b239e77f123efe93b4486f784
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-03-19 17:52:03 +0800

    Integration products API

--- src/components/CardProduct.vue
+++ src/components/CardProduct.vue
@@ -1,18 +1,18 @@
 <template>
 	<section class="break-inside-avoid w-42 mb-8 rounded-lg bg-white shadow-sm overflow-hidden">
-		<img :src="source.thumb" class="active:scale-95 z-10 duration-300 w-full aspect-square" />
+		<img :src="source.thumbnail" :alt="source.thumbnail" class="active:scale-95 z-10 duration-300 w-full aspect-square" />
 		<div class="py-4 px-2 text-center text-sm">
 			<h1 class="font-medium">{{ source.title }}</h1>
-			<h2 class="font-medium">{{ source.prefix }} {{ source.price.toLocaleString('id') }}</h2>
+			<h2 class="font-medium">Rp {{ source.price.toLocaleString('id') }}</h2>
 			<p class="text-xs">
 				stok {{ source.stock }}, masih ada <span class="text-red-600">{{ source.stock - source.sold }}</span>
 			</p>
 			<div 
 				:class="count > 0 ? 'bg-green-500' : 'bg-gray-400'"
 				class="w-8/12 mx-auto duration-300 flex items-center justify-between px-4 text-gray-100 text-base font-medium rounded-2xl mt-4">
-				<button @click="increment" class="active:scale-95 text-xl"> + </button>
-				{{ count }}
 				<button @click="decrement" class="active:scale-95 text-xl"> - </button>
+				{{ count }}
+				<button @click="increment" class="active:scale-95 text-xl"> + </button>
 			</div>
 		</div>
 	</section>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,15 +3,15 @@
 		<HeaderProducts :title="dropPointName" />
 
 		<section class="columns-2 mt-40">
-			<div @click="toOrders" class="active:scale-95 duration-300 w-44 flex items-center gap-2 mb-5 bg-orange-500 rounded-xl px-3 py-2 text-gray-50">
+			<div @click="toOrders" class="active:scale-95 duration-300 w-42 flex items-center gap-4 mb-5 bg-orange-500 rounded-xl px-3 py-2 text-gray-50">
 				<i class="fa fa-user"></i>
-				<p class="text-base">42 customer telah order</p>
+				<p class="text-base">{{ ordersCount }} customer telah order</p>
 			</div>
-			<template v-for="card in exampleCard" :key="card.id">
+			<template v-for="card in products" :key="card.id">
 				<CardProduct :source="card" />
 			</template>
 		</section>
-
+		
 		<BottomSheet v-on:close="closeBottomSheet" :show="isShow" :order="order" />
 		<FloatingOrder v-on:order="showBottomSheet" />
 	</main>	
@@ -20,7 +20,8 @@
 <script setup>
 
 	import { useRoute, useRouter } from 'vue-router'
-	import { computed, ref } from 'vue'
+	import { computed, ref, onMounted } from 'vue'
+	import http from '@/http'
 	import HeaderProducts from '@/components/HeaderProducts.vue'
 	import CardProduct from '@/components/CardProduct.vue'
 	import BottomSheet from '@/components/BottomSheet.vue'
@@ -29,6 +30,21 @@
 	const route = useRoute()
 	const router = useRouter()
 	const dropPointName = computed(() => route.params.dropPointName)
+
+	//Get products and orders count
+	const products = ref([])
+	const ordersCount = ref(0)
+	onMounted(() => {
+		http.get('/products/ready-stock', response => {
+			if (response.status) products.value = response.results.products
+			else alert(response)
+		})
+
+		http.get('/orders/count', response => {
+			if (response.status) ordersCount.value = response.results.count
+			else alert(response)
+		})
+	})
 
 	const isShow = ref(false)
 	let order = ref({
